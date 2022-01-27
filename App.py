c15c71edfd71e7ae4edde5288bfd7ed926930d6c
IceShell101 <80981389+IceShell101@users.noreply.github.com>
2022-01-27 18:33:04 +0000

    Add files via upload

--- App.py
+++ App.py
@@ -1,8 +1,4 @@
-import os
-import sys
-import shutil
-import webbrowser
-import json
+import os, sys, shutil, webbrowser, json
 
 SERVING_TYPE  = "1" 
 PATH = None
@@ -19,81 +15,87 @@ from flask import Flask , render_template , send_file , request , redirect
 def file_type(string):
     dt = {
         "txt" : ['.txt' , '.py' , '.cpp' , '.c++'] ,
-        "img" : ['.jpg' , '.png' , '.gif' , '.webp'],
+        "img" : ['.jpg', 'jpeg' , '.png' , '.gif' , '.webp'],
         "video" : ['.webm' ,'.mp4',".mov"],
         }
-
-    a = (string.lower()).split(".")
-    b = '.'+ a[-1]
-    c = []
+    b = '.'+ (string.lower()).split(".")[-1]
     for i in list(dt):
-        if b in dt[i]:
-            return i
-        else:
-            c.append(False)
-    if int(len(c)) == 3:
-        return None
-def w_is_url(string):
-    text = string.split()
-    b = [i for i in text if "://" in i and int(len(i.split("."))) == 2 or int(len(i.split("."))) >= 2]
-    result = [result.append(f'<a href="{i}" style="color:blue;" target="blank">{i}</a>') for i in text if i in b] #else result.append(i)]
+        if b in dt[i]:return i
+    return None
 
-    return "\n".join(result)
-folder_path =  PATH
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "uploads"
 @app.route("/" , methods=['GET' , 'POST'])
 def home():
-    files = sorted(os.listdir(folder_path))
+    files = sorted(os.listdir(PATH))
     files_data = [{'name':f'{i}' , 'path':f'/{i}' ,  'type':f'{file_type(i)}'} for i in files]
-
+    files_folders_count = {'files':[] , 'folders':[]}
+    for i in files_data:
+        if os.path.isfile(os.path.join(PATH , i['path'].strip('/'))):files_folders_count['files'].append(i)
+        else:files_folders_count['folders'].append(i)
+    files_folders_count['files'] = len(files_folders_count['files'])
+    files_folders_count['folders'] = len(files_folders_count['folders']) 
     if request.method == 'POST':
         if request.form.get("request") == "redierct_request":return redirect(request.form.get("url"))
-        if request.form.get("request") == "create_folder_request" and MODE == "admin":file_d_path = request.form.get("path").strip("/");dirname = request.form.get("dirname");os.mkdir(f"/{file_d_path}/{dirname}")
+        if request.form.get("request") == "create_folder_request" and MODE == "admin":
+            file_d_path = request.form.get("path").strip("/")
+            dirname = request.form.get("dirname")
+            os.mkdir(f"/{file_d_path}/{dirname}")
         if request.form.get("request") == "delete_request" and MODE == "admin":
             file_d_path = (request.form.get("file_path")).lstrip("/")
             file_copy = file_d_path.split("/")
-            if os.path.isfile(file_d_path):shutil.copyfile(f"/{file_d_path}", f"Trash/{file_copy[-1]}");os.remove(f"/{file_d_path}")
+            if os.path.isfile(os.path.join(PATH , file_d_path)):
+                shutil.copyfile(os.path.join(PATH , file_d_path) , os.path.join("Trash",file_copy[-1]))
+                os.remove(os.path.join(PATH , file_d_path))
             else:os.rmdir(f"/{file_d_path}")
             return redirect(request.form.get("path"))
         elif request.form.get("request") == "upload_request" and MODE == "admin":
             def upload_m():
                 a = request.files['file']
-                b= request.form.get("path").strip("/")
-                if int(len(b)) != 0 and int(len(a.filename)) != 0:
-                    a.save(f"/{b}/{a.filename}")
-                elif int(len(b)) == 0 and int(len(a.filename)) > 0:
-                    a.save(f"/{a.filename}")
+                b = (request.form.get("path")).strip("/")
+                if os.path.exists(os.path.join(PATH , b , a.filename)):
+                    count_c = 1
+                    if os.path.exists(os.path.join(PATH,b,f"({count_c})"+a.filename)):
+                        while os.path.exists(os.path.join(PATH,b,f"({count_c})"+a.filename)):count_c +=1
+                        a.save(os.path.join(PATH,b,f"({count_c})"+a.filename))
+                    else: a.save(os.path.join(PATH,b,f"({count_c})"+a.filename))
+                else:a.save(os.path.join(PATH , b , a.filename))
             up_m = Process(target=upload_m)
             up_m.start()
             return redirect(request.form.get("path"))
         elif request.form.get("request") == "openurl_request" and MODE == "admin":
             webbrowser.open(request.form.get("url_text"))
         elif request.form.get("request") == "shell_request" and MODE == "admin":os.system(request.form.get("shell_command"))
-        else:
-            return redirect(request.form.get("path"))
-    return render_template("index.html" , files_data=files_data , back_button_path="/" , current_path=request.path , usermode=MODE , files_num=len(files_data))
+        else:return redirect(request.form.get("path"))
+    return render_template("index.html" , files_data=files_data , back_button_path="/" , current_path=request.path , usermode=MODE , files_num=files_folders_count)
 
 @app.errorhandler(404)
 def error404(e):
         myurl = request.path
         try:
             try:
-                file_c = sorted(os.listdir(f"{folder_path}/{myurl}"))
+                file_c = sorted(os.listdir(os.path.join(PATH,*(myurl.split('/')))))
                 files_data = [ {'name':f'{i}' , 'path':f'{myurl}/{i}' ,  'type':f'{file_type(i)}'}  for i in file_c]
+                files_folders_count = {'files':[] , 'folders':[]}
+                for i in files_data:
+                    if os.path.isfile(os.path.join(PATH ,i['path'].strip('/'))):
+                        files_folders_count['files'].append(i)
+                    else:
+                        files_folders_count['folders'].append(i)
+                files_folders_count['files'] = len(files_folders_count['files'])
+                files_folders_count['folders'] = len(files_folders_count['folders'])
                 back_button_path = "/".join(myurl.split('/')[:-1])
                 if back_button_path == "":
                     back_button_path = "/"
                 print(f"visitor:{request.remote_addr}")
-                return render_template("index.html" , files_data=files_data , back_button_path=back_button_path , current_path=request.path , usermode=MODE , files_num=len(files_data))
+                return render_template("index.html" , files_data=files_data , back_button_path=back_button_path , current_path=request.path , usermode=MODE , files_num=files_folders_count)
             except:
-                if file_type(f"{folder_path}/{myurl}") == 'txt':return send_file(f"{folder_path}/{myurl}")
-                else:return send_file(f"{folder_path}/{myurl}")
+                if file_type(myurl) == 'txt':return send_file(os.path.join(PATH,myurl))
+                else:return send_file(os.path.join(PATH , myurl))
         except:return render_template("error404.html" , myurl=myurl )  , 404
+
 @app.errorhandler(500)
-def error500(e):
-    return "SERVER ERROR"
-app.logger.disabled = True
+def error500(e):return "error 500 , (SERVER ERROR)"
 if SERVING_TYPE == "2":app.run(port=PORT , host='0.0.0.0')
 else:app.run(port=PORT)
 
